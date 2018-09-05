99f417bf1f31d6492cc38439c46afd73b952b7ad
Peter Corke <peter.i.corke@gmail.com>
2018-09-05 09:59:36 -0400

    clarify doco

--- stl/stllog.m
+++ stl/stllog.m
@@ -5,7 +5,7 @@
  % string to the log where it is displayed with date, time and thread name.
  %
  % NOTES::
- % - String arguments must be wrapped with cstring, eg. cstring('hello')
+ % - String arguments, not the format string, must be wrapped with cstring, eg. cstring('hello')
     coder.cinclude('stl.h');
     
     coder.ceval('stl_log', cstring(varargin{1}), varargin{2:end} ); % evaluate the C function
