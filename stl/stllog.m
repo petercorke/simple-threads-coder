3bb4ac77d7f67961a3a50889559074975ecd67cc
Peter Corke <peter.i.corke@gmail.com>
2018-09-05 08:03:55 -0400

    doco

--- stl/stllog.m
+++ stl/stllog.m
@@ -1,4 +1,11 @@
  function stllog(varargin)
+ %stl.log Send formatted string to log
+ %
+ % stl.log(fmt, args...) has printf() like semantics and sends the formatted
+ % string to the log where it is displayed with date, time and thread name.
+ %
+ % NOTES::
+ % - String arguments must be wrapped with cstring, eg. cstring('hello')
     coder.cinclude('stl.h');
     
     coder.ceval('stl_log', cstring(varargin{1}), varargin{2:end} ); % evaluate the C function
