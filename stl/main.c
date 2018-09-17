58ee35ce847220f5a3f3f3a2468dea61a6e886bf
Peter Corke <peter.i.corke@gmail.com>
2018-09-16 20:56:38 -0400

    fixes for stack data

--- stl/main.c
+++ stl/main.c
@@ -12,15 +12,34 @@
 #include    "user_types.h"
 #include    "stl.h"
 
+#ifdef typedef_userStackData
+userStackData  SD;
+#endif
+#ifdef typedef_userPersistentData
+userPersistentData pd;
+#endif
+
 int 
 main(int argc, char **argv)
 {
     // initialize the thread library    
     stl_initialize(argc, argv);
 
-    // call the user's function
+    // initialize stack data required for some MATLAB functions
+#ifdef typedef_userStackData
+    #ifdef typedef_userPersistentData
+    	SD.pd = &pd;
+    #endif
+    user_initialize(&SD);
+#else
     user_initialize();
-    user();
+#endif
+
+    // call the user's function
+    user(); 
+    // user(&SD);  use this if user has a stack pointer argument
+
+
     user_terminate();
     
     return 0;
